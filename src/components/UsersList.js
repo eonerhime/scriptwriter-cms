3d4215de785ce0641a38bea9e9e31cb69be4ef0b
Emo Onerhime <emo.onerhime@gmail.com>
2025-04-21 22:13:55 +0100

    Enhance UsersList component layout by adjusting spacing and improving user interface elements for better readability and usability

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -50,74 +50,76 @@ export default function UsersList({ slug, initialData, roles }) {
 
   return (
     <div className="overflow-y-auto h-[calc(100vh-12rem)] p-6 scrollbar-thin scrollbar-thumb-gray-400">
-      <div className="w-fit mb-4 ">
+      <div className="w-fit mb-8">
         <Button onClick={handleCreateNewUser}>Create New User</Button>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {users.map((user, index) => (
-          <div key={user?.id} className="w-full h-full">
-            <button
-              onClick={() => handleUserClick(user)}
-              className="h-full w-full border-1 p-4 rounded-lg border-accent-950 cursor-pointer flex flex-col justify-between"
-            >
-              {/* Hidden ID or marker */}
-              <input type="hidden" name={`id_${index}`} value={user?.id} />
-
-              {/* User Name */}
-              <div className="flex flex-col gap-2 mb-2">
-                <label
-                  htmlFor={`fullName_${index}`}
-                  className="capitalize text-lg font-semibold"
-                >
-                  {user.fullName}
-                </label>
-              </div>
-
-              {/* User Image */}
-              <div className="flex flex-col gap-3 mb-4">
-                <div className="w-full h-64 relative">
-                  <Image
-                    src={user.avatar_url}
-                    alt="User avatar"
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-md"
-                  />
-                </div>
-              </div>
-
-              {/* User Email */}
-              <div className="flex flex-col gap-2 mb-3 text-center ">
-                <label htmlFor={`email_${index}`} className="text-sm">
-                  {user.email}
-                </label>
-              </div>
-
-              {/* User Role */}
-              <div className="flex font-bold flex-col text-center capitalize gap-2 mb-3">
-                <label htmlFor={`role_${index}`} className="text-sm">
-                  {user.role}
-                </label>
-              </div>
-            </button>
-
-            {/* Delete Checkbox */}
-            <div
-              htmlFor={`delete_${index}`}
-              className="flex items-center gap-2 mt-4 mb-4"
-            >
-              <input
-                type="checkbox"
-                id={`delete_${index}`}
-                name={`delete_${index}`}
-                className="cursor-pointer"
-              />
-              <label
-                htmlFor={`delete_${index}`}
-                className="text-sm text-red-600 font-semibold"
+          <div className="mb-8">
+            <div key={user?.id} className="w-full h-full">
+              <button
+                onClick={() => handleUserClick(user)}
+                className="h-full w-full border-1 p-4 rounded-lg border-accent-950 cursor-pointer flex flex-col justify-between"
               >
-                Mark for deletion
-              </label>
+                {/* Hidden ID or marker */}
+                <input type="hidden" name={`id_${index}`} value={user?.id} />
+
+                {/* User Name */}
+                <div className="flex flex-col gap-2 mb-2">
+                  <label
+                    htmlFor={`fullName_${index}`}
+                    className="capitalize text-lg font-semibold"
+                  >
+                    {user.fullName}
+                  </label>
+                </div>
+
+                {/* User Image */}
+                <div className="flex flex-col gap-3 mb-4">
+                  <div className="w-full h-64 relative">
+                    <Image
+                      src={user.avatar_url}
+                      alt="User avatar"
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-md"
+                    />
+                  </div>
+                </div>
+
+                {/* User Email */}
+                <div className="flex flex-col gap-2 mb-3 text-center ">
+                  <label htmlFor={`email_${index}`} className="text-sm">
+                    {user.email}
+                  </label>
+                </div>
+
+                {/* User Role */}
+                <div className="flex font-bold flex-col text-center capitalize gap-2 mb-3">
+                  <label htmlFor={`role_${index}`} className="text-sm">
+                    {user.role}
+                  </label>
+                </div>
+              </button>
+
+              {/* Delete Checkbox */}
+              <div
+                htmlFor={`delete_${index}`}
+                className="flex items-center gap-2 mt-2"
+              >
+                <input
+                  type="checkbox"
+                  id={`delete_${index}`}
+                  name={`delete_${index}`}
+                  className="cursor-pointer"
+                />
+                <label
+                  htmlFor={`delete_${index}`}
+                  className="text-sm text-red-600 font-semibold"
+                >
+                  Mark for deletion
+                </label>
+              </div>
             </div>
           </div>
         ))}
